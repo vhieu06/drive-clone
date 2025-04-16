c126c3447035283575bc93317de9c041892694b8
Vhieu06 <Hieuduycuonganh6avip@gmail.com>
2025-04-16 20:58:06 +0700

    Initial commit

--- app.js
+++ app.js
@@ -211,4 +211,5 @@ app.post("/verify-otp", async (req, res) => {
   });
 });
 
-app.listen(3000, () => console.log("Server started on http://localhost:3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server started on http://localhost:${PORT}`));
